798171d18915f4648aa2c0d127d71d2b60634419
Jeremy Likness <jeremy@jeremylikness.com>
2018-08-31 14:46:08 -0700

    Fix base uri

--- static-app/src/app/bifurcation.service.ts
+++ static-app/src/app/bifurcation.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 const BIFURC_URI: string = "https://localhost:5001/api/Bifurc";
-const BIFURC_FN_URI: string = "https://ngfunction.azurewebsites.net/api/Bifurc";
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +16,6 @@ export class BifurcationService {
      let params = new HttpParams()
       .set('skip', `${skip}`)
       .set('iterations', `${iterations}`);
-     return this.http.get<number[]>(`${BIFURC_FN_URI}/${r}/`, { params });
+     return this.http.get<number[]>(`${BIFURC_URI}/${r}/`, { params });
    }
 }
